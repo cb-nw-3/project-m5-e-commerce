e04f2bceded8b2a6939d53b6dc0aac0ff0bc1fbf
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-10 11:46:04 -0400

    add request for all items

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const allItems = require('./js/allItems');
+
 const PORT = 4000;
 
 express()
@@ -25,6 +27,6 @@ express()
   .use('/', express.static(__dirname + '/'))
 
   // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  .get('/items', allItems)
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
