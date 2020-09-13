c3b9c1cc1e8217e726b97a4f8b6160bcb13ab4bc
Aquarius-beans <rare.beans1959@gmail.com>
2020-09-13 15:16:33 -0400

    item grid wrapper working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import ItemGridWrapper from "./ItemGridWrapper";
 
 function App() {
-  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch("/items")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/items")
+  //     .then((response) => response.json())
+  //     .then((data) => setItems(data));
+  // }, []);
 
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item._id}>{item.name}</li>
-      ))}
-    </ul>
+    <ItemGridWrapper />
+    // <UL>
+    //   {items.map((item) => (
+    //     <li key={item._id}>{item.name}</li>
+    //   ))}
+    // </UL>
   );
 }
+
+// const UL = styled.ul`
+//   list-style-type: none;
+// `;
 
 export default App;
---
+++ client/src/components/ItemGridWrapper.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+
+const ItemGridWrapper = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/items")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <Wrapper>
+      {items.map((item) => (
+        <li key={item._id}>{item.name}</li>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  grid-gap: 24px;
+`;
+
+export default ItemGridWrapper;
