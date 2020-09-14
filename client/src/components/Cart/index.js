429048764a39632b7aa2abf0887e04571570eff0
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-13 21:38:05 -0400

    homepage sketch

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,19 +4,9 @@ import CartHeader from "./CartHeader";
 import CartBody from "./CartBody";
 import CartFooter from "./CartFooter";
 
-const Cart = ({ bacon }) => {
-  const storeArray = bacon.map((item) => {
-    return (
-      <div>
-        <h1>{item.name}</h1>
-        <img src={item.imageSrc}></img>
-      </div>
-    );
-  });
-  console.log(storeArray);
+const Cart = () => {
   return (
     <CartContainer>
-      {storeArray}
       <CartHeader />
       <CartBody />
       <CartFooter />
