839823abfd52ae8a57c58ac57aa5051ffef938ee
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-09-16 14:38:57 -0400

    Purchase button functional

--- client/src/components/Cart/CartFooter.js
+++ client/src/components/Cart/CartFooter.js
@@ -1,30 +1,37 @@
 import React from "react";
 import styled from "styled-components";
-
+import { getCartItemArray } from "../reducers/index";
+import { useSelector } from "react-redux";
 
 const CartFooter = () => {
+  const cartItemNewState = useSelector(getCartItemArray);
+  const [total, setTotal] = React.useState(0);
   const handleCartPurchase = (event) => {
     event.preventDefault();
-    fetch('/cartItems', {
-      method: 'POST',
+    fetch("/cartItems", {
+      method: "POST",
       body: JSON.stringify({
-        // Cart object
+        cartItemNewState,
       }),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     })
-    .then(res => res.json())
-    .then(data => {
-    })
-    .catch(err => console.log(err))
+      .then((res) => res.json())
+      .then((totalobject) => {
+        setTotal(totalobject.status);
+      })
+
+      .catch((err) => console.log(err));
   };
 
   return (
     <CartFooterContainer>
-      <form><StyledButton>Purchase</StyledButton></form>
-      <PriceTotal>$00.00</PriceTotal>
+      <form>
+        <StyledButton onClick={handleCartPurchase}>Purchase</StyledButton>
+      </form>
+      <PriceTotal>${total}</PriceTotal>
     </CartFooterContainer>
   );
 };
