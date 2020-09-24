f212863d63f18d6ba397cd2fe5803e7c62925c50
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-24 11:21:45 -0400

    Loading spinner

--- client/src/components/LoadingIcon/index.js
+++ client/src/components/LoadingIcon/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 const LoadingIcon = () => {
-  return <p>Loading...</p>
-}
+  let firstRandomBagel = Math.floor(Math.random() * 3) + 1;
+  let secondRandomBagel = Math.floor(Math.random() * 3) + 1;
+  return (
+    <>
+      <RotatingImage src={require(`../assets/bagel${firstRandomBagel}.png`)} />
+      <RotatingImage src={require(`../assets/bagel${secondRandomBagel}.png`)} />
+    </>
+  );
+};
+
+const RotatingAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const RotatingImage = styled.img`
+  width: 100px;
+  height: 100px;
+  animation: ${RotatingAnimation} 1.5s infinite linear;
+  margin: 0 auto;
+`;
 
 export default LoadingIcon;
