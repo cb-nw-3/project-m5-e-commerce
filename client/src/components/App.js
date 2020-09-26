98fa6df92fc670290b75ab9a441887335d97d4e5
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-09-26 16:04:06 -0400

    merch page added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import ProductDetails from "./Shopping/ProductDetails";
 import HomePage from "./HomePage/index";
 import GlobalStyles from "./GlobalStyles";
 import AboutUs from "./AboutUs.js/index";
-import Fashion from './Fashion/index'
+import Fashionspace from './Fashion/index'
 
 const App = () => {
   return (
@@ -19,8 +19,8 @@ const App = () => {
           <Route exact path="/shop/:itemId">
             <ProductDetails />
           </Route>
-          <Route exact path="/merch">
-            <Fashion />
+          <Route exact path="/fashion">
+            <Fashionspace />
           </Route>
         </Switch>
       <GlobalStyles />
