469d3842173f02f525c29aa20b6a323e3e190d06
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-27 16:35:38 -0400

    merch page simplify

--- client/src/components/Fashion/Fashion.js
+++ client/src/components/Fashion/Fashion.js
@@ -1,27 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
-
-const Fashion= ({imgSrc}) => {
-return (
+const Fashion = ({ imgSrc }) => {
+  return (
     <Container>
-    <Image src={imgSrc} />
-  </Container>
-
-);
+      <Image src={imgSrc} />
+    </Container>
+  );
 };
 
-export default Fashion;
-
 const Container = styled.div`
-  position: relative;
-  text-align: right;
-  color: white;
-  font-size: 50px;
-  font-family: "Montserrat", sans-serif;
+  text-align: center;
 `;
 
 const Image = styled.img`
-  width: 100%;
-  display: block;
+  width: 75%;
 `;
+
+export default Fashion;
